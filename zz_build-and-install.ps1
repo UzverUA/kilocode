b59a2aa7a8ddd4ea970142eb9abe28f49d28b275
Taras <tt8871@gmail.com>
2025-11-25 04:43:38 +0200

    fix(scripts): improve output formatting in build and update scripts

--- zz_build-and-install.ps1
+++ zz_build-and-install.ps1
@@ -3,16 +3,20 @@
 # Works on Windows + PowerShell 5.1 or newer (default on Windows 10/11)
 # ========================
 
+function Green  { Write-Host $_ -ForegroundColor Green }
+function Yellow { Write-Host $_ -ForegroundColor Yellow }
+function Red    { Write-Host $_ -ForegroundColor Red }
+
 # 1. Build the new VSIX
-Write-Host "==================`n" -ForegroundColor Cyan
-Write-Host "=== pnpm build ===`n" -ForegroundColor Cyan
+Write-Host "==================" -ForegroundColor Cyan
+Write-Host "=== pnpm build ===" -ForegroundColor Cyan
 Write-Host "==================`n" -ForegroundColor Cyan
 pnpm build
 
 
 # 2. Find the newest VSIX and install it automatically into VS Code
-Write-Host "=============================`n" -ForegroundColor Cyan
-Write-Host "=== Installing extensions ===`n" -ForegroundColor Cyan
+Write-Host "=============================" -ForegroundColor Cyan
+Write-Host "=== Installing extensions ===" -ForegroundColor Cyan
 Write-Host "=============================`n" -ForegroundColor Cyan
 $vsix = Get-ChildItem "bin\*.vsix" | Sort-Object Name -Descending | Select-Object -First 1
 if (!$vsix) {
@@ -27,7 +31,7 @@ if ($LASTEXITCODE -eq 0) {
     Red "VS Code failed to install the extension (is VS Code running?)"
 }
 
-Write-Host "============`n" -ForegroundColor Cyan
-Write-Host "=== DONE ===`n" -ForegroundColor Cyan
-Write-Host "============`n" -ForegroundColor Cyan
+Write-Host "============" -ForegroundColor Green
+Write-Host "=== DONE ===" -ForegroundColor Green
+Write-Host "============`n" -ForegroundColor Green
 pause
